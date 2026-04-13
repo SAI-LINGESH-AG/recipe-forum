121816bc290682bbe4ded23a7a1c11a2251d0447
SL <sailingesh664@gmail.com>
2026-04-13 11:11:33 +0530

    deleted migration file

--- supabase/migrations/20260411140000_add_recipe_video_url.sql
+++
@@ -1,6 +0,0 @@
--- Optional recipe video link (YouTube, Drive, Vimeo, direct file URL, etc.)
-
-alter table public.recipes
-  add column if not exists video_url text null;
-
-comment on column public.recipes.video_url is 'Optional public URL for a recipe video (embed when supported).';
